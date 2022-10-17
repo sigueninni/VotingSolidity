6b358074631594bc4ecab5543ae2ce40da4491c6
Kywan Labs <67273768+sigueninni@users.noreply.github.com>
2022-10-17 13:56:35 +0200

    Update Voting.sol

--- Voting.sol
+++ Voting.sol
@@ -5,6 +5,7 @@ pragma solidity 0.8.17;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 /// @title Voting
+
 contract Voting is Ownable {
     //Workflow status
     enum WorkflowStatus {
